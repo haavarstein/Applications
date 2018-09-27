0e739a2ae49c1e7fd4d02bf8736cb06162aca8c5
Trond Eirik Haavarstein <haavarstein@gmail.com>
2018-09-27 15:00:04 -0300

    Update Install.ps1

--- Microsoft/Office 365/Install.ps1
+++ Microsoft/Office 365/Install.ps1
@@ -1,19 +1,42 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
 $Vendor = "Microsoft"
-$Product = "Office 365"
+$Product = "Office 365 x32"
 $PackageName = "setup"
-$Version = "15.0.4805.1003"
+$Version = "16.0.10810.33603"
 $InstallerType = "exe"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
 $UnattendedArgs = '/configure RDSH.xml'
+$UnattendedArgs2 = '/download RDSH.xml'
 
 Start-Transcript $LogPS
 
 CD $Version
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $PSScriptRoot\$Version\Office\Data\v32.cab)) {
+    (Start-Process "Setup.exe" -ArgumentList $unattendedArgs2 -Wait -Passthru).ExitCode
+         }
+        Else {
+            Write-Verbose "File exists. Skipping Download." -Verbose
+         }
 
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
 (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
