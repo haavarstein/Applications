8895da116cfd0279fc0bb6b76d18e2a63c2838fb
Trond Eirik Haavarstein <haavarstein@gmail.com>
2018-10-08 08:33:05 -0300

    Update Install.ps1

--- Misc/BISF/Install.ps1
+++ Misc/BISF/Install.ps1
@@ -23,12 +23,14 @@ $Version = "6.1.0.01.101"
 $InstallerType = "exe"
 $Source = "$PackageName" + "." + "zip"
 $SourceXML = "$PackageName" + "." + "zip"
+$SourceCTX = "CitrixOptimizer.zip"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
 $UnattendedArgs = "/VERYSILENT /log:$LogApp /norestart /noicons"
 $url = "http://loginmarketing.blob.core.windows.net/public/tools/setup-BIS-F-6.1.0_build01.101.exe.zip"
 $xml = "https://eucweb.com/download/765/"
+$ctx = "http://xenapptraining.s3.amazonaws.com/Hydration/CitrixOptimizer.zip"
 $ProgressPreference = 'SilentlyContinue'
 
 Start-Transcript $LogPS
@@ -45,9 +47,14 @@ If (!(Test-Path -Path $Source)) {
     Invoke-WebRequest -Uri $url -OutFile $Source
     Expand-Archive -Path $Source -DestinationPath .\
     Remove-Item -Path $Source
+    Write-Verbose "Downloading $Vendor $Product Reference Configuration" -Verbose
     Invoke-WebRequest -Uri $xml -OutFile $SourceXML
     Expand-Archive -Path $SourceXML -DestinationPath .\
     Remove-Item -Path $SourceXML
+    Write-Verbose "Downloading Citrix Optimizer" -Verbose
+    Invoke-WebRequest -Uri $ctx -OutFile $SourceCTX
+    Expand-Archive -Path $SourceCTX -DestinationPath .\Tools\
+    Remove-Item -Path $SourceCTX
          }
         Else {
             Write-Verbose "File exists. Skipping Download." -Verbose
@@ -57,6 +64,8 @@ Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
 (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
 
 Write-Verbose "Customization" -Verbose
+Copy-Item -Path .\Tools\* -Destination $env:SystemRoot\System32 -Recurse -Force
+Copy-item -Path .\*.xml -Destination "C:\Program Files (x86)\Base Image Script Framework (BIS-F)" -Recurse -Force
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
