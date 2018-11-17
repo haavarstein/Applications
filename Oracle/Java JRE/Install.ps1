ca0becaeb593d0be03ccb8aa20c2a48d9bde0e98
Trond Eirik Haavarstein <haavarstein@gmail.com>
2018-11-17 08:08:25 -0300

    Create Install.ps1

---
+++ Oracle/Java JRE/Install.ps1
@@ -0,0 +1,57 @@
+# https://www.reddit.com/r/PowerShell/comments/4fgbv4/powershell_function_to_get_latest_java_release/
+# All Credits to gangstanthony
+
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+$Vendor = "Oracle"
+$Product = "Java SE Runtime Environment"
+$PackageName = "JRE_x64"
+$Page = Invoke-WebRequest http://java.com/en/download/windows_offline.jsp
+$Version = $page.RawContent -split "`n" | ? {$_ -match 'recommend'} | select -f 1 | % {$_ -replace '^[^v]+| \(.*$'}
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+$url = $page.links.href | ? {$_ -match '^http.*download'} | select -f 1
+$ProgressPreference = 'SilentlyContinue'
+$UnattendedArgs = '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0'
+
+Start-Transcript $LogPS
+
+if( -Not (Test-Path -Path $Version ) )
+{
+    New-Item -ItemType directory -Path $Version
+}
+
+CD $Version
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {
+    Invoke-WebRequest -Uri $url -OutFile $Source
+         }
+        Else {
+            Write-Verbose "File exists. Skipping Download." -Verbose
+         }
+
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
