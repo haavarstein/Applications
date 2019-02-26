23fb360e8baf940df47a346c0ac26e27cd3dfdd2
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-02-26 09:51:29 -0300

    Update Install.ps1

--- Adobe/Reader DC/Install.ps1
+++ Adobe/Reader DC/Install.ps1
@@ -1,10 +1,10 @@
-# Bronson (c) 2018 This will download the Adobe Stuff
-
+# Bronson Magnan - https://twitter.com/CIT_Bronson - This will download the Adobe Stuff
+ 
 $ftp = "ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/"
-
+ 
 # We have to use .NET to read a directory listing from FTP, it is different than downloading a file.
 # Original C# code at https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-list-directory-contents-with-ftp
-
+ 
 $request = [System.Net.FtpWebRequest]::Create($ftp);
 $request.Credentials = [System.Net.NetworkCredential]::new("anonymous", "password");
 $request.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails;
@@ -14,10 +14,10 @@ $request.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails;
 $DirList = $reader.ReadToEnd()
 $reader.Close()
 $response.close()
-
+ 
 # Split into Lines, currently it is one big string.
 $DirByLine = $DirList.split("`n")
-
+ 
 # Get the token containing the folder name.
 $folders = @()
 foreach ($line in $DirByLine ) { 
@@ -27,7 +27,7 @@ foreach ($line in $DirByLine ) {
         $folders += $endtoken
     }
 }
-
+ 
 # Sort the folders by newest first, and select the first 1, and remove the newline whitespace at the end.
 $currentfolder = ($folders | sort -Descending | select -First 1).trim()
 
@@ -50,34 +50,51 @@ $Product = "Reader DC"
 $PackageName = "AcroRdrDC"
 $Version = "$currentfolder"
 $InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
 $UnattendedArgs = '/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES'
 $ProgressPreference = 'SilentlyContinue'
 
-Start-Transcript $LogPS
+Start-Transcript $LogPS | Out-Null
 
-if ( -Not (Test-Path -Path $Version ) ) {
-    New-Item -ItemType directory -Path $Version
+Write-Verbose "Checking Internet Connection" -Verbose
+
+If (!(Test-Connection -ComputerName www.google.com -Count 1 -quiet)) {
+    Write-Verbose "Internet Connection is Down" -Verbose
+    }
+    Else {
+    Write-Verbose "Internet Connection is Up" -Verbose
+    }
+
+if (!$Version) {
+    $Version | Out-File -FilePath ".\Version.txt" -Force
+else {
+    $Version = Get-Content -Path ".\Version.txt"
+    }
 }
 
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-$EXEDownload = "$($ftp)$($currentfolder)`/AcroRdrDC$($currentfolder)_en_US.exe"
-$filename = ($EXEDownload.split("/"))[-1]
+if( -Not (Test-Path -Path $Version ) )
+{
+    New-Item -ItemType directory -Path $Version | Out-Null
+    $Version | Out-File -FilePath ".\Version.txt" -Force
+}
 
 CD $Version
 
-If (!(Test-Path -Path $filename)) {
+If (!(Test-Path -Path $Source)) {
     Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
-    invoke-webrequest -UseBasicParsing -uri $EXEDownload -outfile .\$filename
+    $EXEDownload = "$($ftp)$($currentfolder)`/AcroRdrDC$($currentfolder)_en_US.exe"
+    $filename = ($EXEDownload.split("/"))[-1]
+    wget -uri $EXEDownload -outfile $Source
 }
 Else {
-    Write-Verbose "File exists. Skipping Download." -Verbose
+    Write-Verbose "File Exists. Skipping Download." -Verbose
 }
 
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
-(Start-Process "$filename" $UnattendedArgs -Wait -Passthru).ExitCode
+(Start-Process "$Source" $UnattendedArgs -Wait -Passthru).ExitCode
 
 Write-Verbose "Customization" -Verbose
 Unregister-ScheduledTask -TaskName "Adobe Acrobat Update Task" -Confirm:$false
@@ -89,4 +106,4 @@ Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
 Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
 Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
-Stop-Transcript
+Stop-Transcript  | Out-Null
