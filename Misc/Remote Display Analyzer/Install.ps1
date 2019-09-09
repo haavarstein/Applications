bc9173d2fc3450a3f5a06a6dadcfdff1c9d20cc8
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-09-09 11:32:23 -0300

    Create Install.ps1

---
+++ Misc/Remote Display Analyzer/Install.ps1
@@ -0,0 +1,55 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+$Vendor = "Misc"
+$Product = "Remote Display Analyzer"
+$PackageName = "RemoteDisplayAnalyzer"
+$Version = "1903"
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+$UnattendedArgs = '/qn'
+$url = "https://www.rdanalyzer.com/wp-content/uploads/2019/03/RDA1903.zip"
+$ProgressPreference = 'SilentlyContinue'
+
+Start-Transcript $LogPS
+
+if( -Not (Test-Path -Path $Version ) )
+{
+    New-Item -ItemType directory -Path $Version | Out-Null
+}
+
+CD $Version
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {
+    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile RDA.zip
+    Expand-Archive -Path RDA.zip -DestinationPath .
+    Remove-Item -Path RDA.zip
+         }
+        Else {
+            Write-Verbose "File exists. Skipping Download." -Verbose
+         }
+
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+Copy-Item $Source -Destination C:\Windows\System32
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
