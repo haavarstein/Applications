5a925c9a9de762224b331615b915aab09012640c
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-10-12 11:03:33 -0300

    Update Install.ps1

--- Misc/NotePadPlusPlus/Install.ps1
+++ Misc/NotePadPlusPlus/Install.ps1
@@ -1,25 +1,3 @@
-function get-NPPCurrentVersion {
-    [cmdletbinding()]
-    [outputtype([version])]
-    $Url = "https://notepad-plus-plus.org/downloads/"
-    $Content = Invoke-WebRequest -Uri $url
-    $Matches = ($content.allelements | ? tagName -eq 'a').innerText -match "Current Version \d+\.\d+\.\d+"
-    $ver = [version]::new($Matches[0].TrimStart("Current Version "))
-    Write-Output $ver
-}
-
-function get-NPPCurrentDownloadURL {
-    [cmdletbinding()]
-    [outputtype([string])]
-    param (
-        [validateSet("x86","x64")][string]$Architecture = "x64"
-    )
-    $version = get-NPPCurrentVersion
-    if ("x86" -eq $Architecture) { $archcode = "" } else { $archcode = ".x64" }
-    $url = "http://download.notepad-plus-plus.org/repository/$($version.major).x/$version/npp.$($version).Installer$($archcode).exe"
-    Write-Output $url
-}
-
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
@@ -30,39 +8,37 @@ function get-NPPCurrentDownloadURL {
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
+
+Write-Verbose "Installing Modules" -Verbose
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
 
 $Vendor = "Misc"
 $Product = "NotePadPlusPlus"
 $PackageName = "NotePadPlusPlus_x64"
-$Version = "$(get-NPPCurrentVersion)"
+$Evergreen = Get-NotepadPlusPlus | Where-Object {$_.Architecture -eq "x64"}
+$Version = $Evergreen.Version
+$URL = $Evergreen.uri
 $InstallerType = "exe"
 $Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
-$url = "$(get-NPPCurrentDownloadURL -Architecture x64)"
 $ProgressPreference = 'SilentlyContinue'
 $UnattendedArgs = '/S'
 
-Start-Transcript $LogPS
-
-if( -Not (Test-Path -Path $Version ) )
-{ 
-    New-Item -ItemType directory -Path $Version | Out-Null
-}
-
+Start-Transcript $LogPS | Out-Null
+ 
+If (!(Test-Path -Path $Version)) {New-Item -ItemType directory -Path $Version | Out-Null}
+ 
 CD $Version
-
+ 
 Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
-If (!(Test-Path -Path $Source)) {
-    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source
-         }
-        Else {
-            Write-Verbose "File exists. Skipping Download." -Verbose
-         }
-
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
+        
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
 (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
 
