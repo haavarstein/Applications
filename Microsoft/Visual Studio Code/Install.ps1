4a63e8ce7a78851af68bd266909b801872762f99
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-10-12 08:50:28 -0300

    Added Evergreen Module - Simplified to One liners

--- Microsoft/Visual Studio Code/Install.ps1
+++ Microsoft/Visual Studio Code/Install.ps1
@@ -1,11 +1,23 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
 Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
+
+if( -Not (Get-Module -ListAvailable -Name Evergreen ) ) {Install-Module Evergreen -Force | Import-Module Evergreen}
  
 $Vendor = "Microsoft"
 $Product = "Visual Studio Code"
-$Version = "1.32.3"
+$Evergreen = Get-MicrosoftVisualStudioCode -Platform win32-x64 -Channel stable
+$Version = $Evergreen.Version
 $PackageName = "VSCode_x64"
 $InstallerType = "exe"
 $Source = "$PackageName" + "." + "$InstallerType"
@@ -13,33 +25,25 @@ $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
 $UnattendedArgs = '/verysilent /suppressmsgboxes /mergetasks=!runcode'
-$url = "https://go.microsoft.com/fwlink/?Linkid=852157"
+$uri = $Evergreen.uri
 $ProgressPreference = 'SilentlyContinue'
 
 Start-Transcript $LogPS | Out-Null
  
-if( -Not (Test-Path -Path $Version ) )
-{
-    New-Item -ItemType directory -Path $Version
-}
+If (!(Test-Path -Path $Version ) ) {New-Item -ItemType directory -Path $Version | Out-Null}
  
 CD $Version
  
 Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
-If (!(Test-Path -Path $Source)) {
-    Invoke-WebRequest -Uri $url -OutFile $Source
-         }
-        Else {
-            Write-Verbose "File exists. Skipping Download." -Verbose
-         }
- 
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile $Source}
+        
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
 (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
  
 Write-Verbose "Customization" -Verbose
-CD "C:\Program Files (x86)\Microsoft VS Code\bin\"
-code --install-extension ms-vscode.powershell -force
-code --install-extension coenraads.bracket-pair-colorizer -force
+#CD "C:\Program Files (x86)\Microsoft VS Code\bin\"
+#code --install-extension ms-vscode.powershell -force
+#code --install-extension coenraads.bracket-pair-colorizer -force
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
