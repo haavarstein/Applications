e06a31ccae9dea370d04aec05d2ad99e39878fa4
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-10-17 16:19:37 -0300

    Create Install.ps1

---
+++ Microsoft/Reporting Services Tools/Install.ps1
@@ -0,0 +1,30 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$startDTM = (Get-Date)
+
+$Vendor = "Microsoft"
+$Product = "Reporting Services Tools"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product PS Wrapper.log"
+
+Start-Transcript $LogPS
+
+Write-Verbose "Starting Installation of $Vendor $Product" -Verbose 
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+if (!(Get-Module -ListAvailable -Name ReportingServicesTools)) {Install-Module -Name ReportingServicesTools -AllowClobber}
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
