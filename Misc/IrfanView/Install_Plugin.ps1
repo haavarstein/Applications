0b7f6339a0cc25aa4c1660e5db137a027f7c31ed
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-10-23 07:56:36 -0300

    Create Install_Plugin.ps1

---
+++ Misc/IrfanView/Install_Plugin.ps1
@@ -0,0 +1,107 @@
+Function Get-IrfanView {    
+    <#
+        .NOTES
+            Author: Trond Eirik Haavarstein
+            Twitter: @xenappblog
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+    Param()
+        $url = "https://www.irfanview.com/"
+    try {
+        $web = Invoke-WebRequest -UseBasicParsing -Uri $url -ErrorAction SilentlyContinue
+    }
+    catch {
+        Throw "Failed to connect to URL: $url with error $_."
+        Break
+    }
+    finally {
+        $m = $web.ToString() -split "[`r`n]" | Select-String "Version" | Select-Object -First 1
+        $m = $m -replace "<((?!@).)*?>"
+        $m = $m.Replace(' ','')
+        $Version = $m -replace "Version"
+        $File = $Version -replace "\.",""
+        $x32 = "http://download.betanews.com/download/967963863-1/iview$($File)_setup.exe"
+        $x32plugin = "http://download.betanews.com/download/1099412658-1/iview$($File)_plugins_setup.exe"
+        $x64 = "http://download.betanews.com/download/967963863-1/iview$($File)_x64_setup.exe"
+        $x64plugin = "http://download.betanews.com/download/1099412658-1/iview$($File)_plugins_x64_setup.exe"
+        
+        $PSObjectx32 = [PSCustomObject] @{
+        Version      = $Version
+        Architecture = "x86"
+        URI          = $x32
+        }
+                      
+        $PSObjectx64 = [PSCustomObject] @{
+        Version      = $Version
+        Architecture = "x64"
+        URI          = $x64
+        }
+
+        $PSObjectx32plugin = [PSCustomObject] @{
+        Version            = $Version
+        Architecture       = "x86 Plugin"
+        URI                = $x32plugin
+        }
+
+        $PSObjectx64plugin = [PSCustomObject] @{
+        Version            = $Version
+        Architecture       = "x64 Plugin"
+        URI                = $x64plugin
+        }
+
+        Write-Output -InputObject $PSObjectx32
+        Write-Output -InputObject $PSObjectx64
+        Write-Output -InputObject $PSObjectx32plugin
+        Write-Output -InputObject $PSObjectx64plugin
+
+    }
+ }
+
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+ 
+$Vendor = "Misc"
+$Product = "IrfanView Plugin"
+$Evergreen = Get-IrfanView | Where-Object {$_.Architecture -eq "x86 Plugin"}
+$Version = $Evergreen.Version
+$URI = $Evergreen.uri
+$PackageName = "IrfanView_Plugin"
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+$UnattendedArgs = '/silent'
+$ProgressPreference = 'SilentlyContinue'
+
+Start-Transcript $LogPS | Out-Null
+ 
+If (!(Test-Path -Path $Version)) {New-Item -ItemType directory -Path $Version | Out-Null}
+ 
+CD $Version
+ 
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile $Source}
+        
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+ 
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript | Out-Null
