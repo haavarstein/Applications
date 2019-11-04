fb054ad9076feaf9fd0349e7d32d1c1e46c283af
Trond Eirik Haavarstein <haavarstein@gmail.com>
2019-11-04 08:43:12 -0300

    Update Install.ps1

--- Adobe/Reader DC/Install.ps1
+++ Adobe/Reader DC/Install.ps1
@@ -1,36 +1,3 @@
-# Bronson Magnan - https://twitter.com/CIT_Bronson - This will download the Adobe Stuff
- 
-$ftp = "ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/"
- 
-# We have to use .NET to read a directory listing from FTP, it is different than downloading a file.
-# Original C# code at https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-list-directory-contents-with-ftp
- 
-$request = [System.Net.FtpWebRequest]::Create($ftp);
-$request.Credentials = [System.Net.NetworkCredential]::new("anonymous", "password");
-$request.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails;
-[System.Net.FtpWebResponse]$response = [System.Net.FtpWebResponse]$request.GetResponse();
-[System.IO.Stream]$responseStream = $response.GetResponseStream();
-[System.IO.StreamReader]$reader = [System.IO.StreamReader]::new($responseStream);
-$DirList = $reader.ReadToEnd()
-$reader.Close()
-$response.close()
- 
-# Split into Lines, currently it is one big string.
-$DirByLine = $DirList.split("`n")
- 
-# Get the token containing the folder name.
-$folders = @()
-foreach ($line in $DirByLine ) { 
-    $endtoken = ($line.split(' '))[-1]
-    #filter out non version folder names
-    if ($endtoken -match "[0-9]") {
-        $folders += $endtoken
-    }
-}
- 
-# Sort the folders by newest first, and select the first 1, and remove the newline whitespace at the end.
-$currentfolder = ($folders | sort -Descending | select -First 1).trim()
-
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
@@ -45,65 +12,41 @@ Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
+Write-Verbose "Installing Modules" -Verbose
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
+Update-Module Evergreen
+
 $Vendor = "Adobe"
 $Product = "Reader DC"
 $PackageName = "AcroRead"
-$Version = "$currentfolder"
-$InstallerType = "msi"
+$Evergreen = Get-AdobeAcrobatReaderDC | Where-Object { $_.Platform -eq "Windows" -and $_.Type -eq "Installer" -and ($_.Language -eq "English" -or $_.Language -eq "Neutral" -or $_.Language -eq "Multi") }
+$Version = $Evergreen.Version
+$URL = $Evergreen.uri
+$InstallerType = "exe"
 $Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
-$UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
-$BaseVersion = "1901220036"
-$Baseurl = "ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1901220036/AcroRdrDC1901220036_en_US.exe"
+$UnattendedArgs='-sfx_nu /sALL /msi EULA_ACCEPT=YES ENABLE_CHROMEEXT=0 DISABLE_BROWSER_INTEGRATION=1 ENABLE_OPTIMIZATION=YES ADD_THUMBNAILPREVIEW=0 DISABLEDESKTOPSHORTCUT=1 UPDATE_MODE=0 DISABLE_ARM_SERVICE_INSTALL=1'
 $ProgressPreference = 'SilentlyContinue'
 
 Start-Transcript $LogPS | Out-Null
-
-if( -Not (Test-Path -Path $BaseVersion ) )
-{
-    New-Item -ItemType directory -Path $BaseVersion | Out-Null
-    CD $BaseVersion
-    Write-Verbose "Downloading $Vendor $Product Base $BaseVersion" -Verbose
-    Invoke-WebRequest -UseBasicParsing -Uri $baseurl -OutFile AcroRdrDC.exe
-    .\AcroRdrDC.exe -sfx_o -sfx_o"C:\Temp\Adobe\1" -sfx_ne
-    Start-Sleep -s 60
-    Copy-Item -Path C:\Temp\Adobe\1\* -Destination .
-    CD..
-}
-
-CD $BaseVersion
-Write-Verbose "Starting Installation of $Vendor $Product Base $BaseVersion" -Verbose
-(Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
-CD..
  
-if( -Not (Test-Path -Path $Version ) )
-{
-    New-Item -ItemType directory -Path $Version | Out-Null
-    $Version | Out-File -FilePath ".\Version.txt" -Force
-}
+If (!(Test-Path -Path $Version)) {New-Item -ItemType directory -Path $Version | Out-Null}
  
 CD $Version
  
-If (!(Test-Path -Path AcroRdrDCUpd$($Version).msp)) {
-    Write-Verbose "Downloading $Vendor $Product MSP Patch $Version" -Verbose
-    $MSPDownload = "$($ftp)$($currentfolder)`/AcroRdrDCUpd$($currentfolder).msp"
-    $filename = ($MSPDownload.split("/"))[-1]
-    wget -uri $MSPDownload -outfile $filename
-}
-Else {
-    Write-Verbose "File Exists. Skipping Download." -Verbose
-}
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
+        
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
 
-Write-Verbose "Patching $Vendor $Product with $Version" -Verbose
-$MSPDownload = "$($ftp)$($currentfolder)`/AcroRdrDCUpd$($currentfolder).msp"
-$filename = ($MSPDownload.split("/"))[-1]
-$UnattendedArgs = "/p $filename /norestart /qn /liewa ${env:SystemRoot}\Temp\$filename.log"
-(Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+Write-Verbose "Customization" -Verbose
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
 Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
 Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
-Stop-Transcript  | Out-Null
+Stop-Transcript
