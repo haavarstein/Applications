e8ce2b3f767f85ba0607805d2c072909146c35e1
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-01-08 20:37:03 -0300

    Update Install.ps1

--- Nutanix/VirtIO/Install.ps1
+++ Nutanix/VirtIO/Install.ps1
@@ -1,9 +1,13 @@
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+
 # Example 3 EXE (Always use ' '):
 # $UnattendedArgs='/qn'
 # (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
 # Example 4 MSI (Always use " "):
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
@@ -13,33 +17,17 @@ $StartDTM = (Get-Date)
 
 $Vendor = "Nutanix"
 $Product = "VirtIO"
-$PackageName = "Nutanix-VirtIO-1.1.3"
-$Version = "1.1.3"
+$PackageName = "Nutanix-VirtIO-1.1.5-64bit"
+$Version = "1.1.5"
 $InstallerType = "msi"
-$Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
 $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
-$url = "http://download.nutanix.com/mobility/1.1.3/Nutanix-VirtIO-1.1.3.msi"
-$ProgressPreference = 'SilentlyContinue'
 
 Start-Transcript $LogPS
 
-if( -Not (Test-Path -Path $Version ) )
-{
-    New-Item -ItemType directory -Path $Version
-}
-
 CD $Version
-
-Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
-If (!(Test-Path -Path $Source)) {
-    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source
-         }
-        Else {
-            Write-Verbose "File exists. Skipping Download." -Verbose
-         }
 
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
 (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
