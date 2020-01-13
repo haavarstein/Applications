0c8660ae4d7cf4d3c014573bc1a00edabf2cae47
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-01-13 12:10:16 -0300

    Create Install.ps1

---
+++ Citrix/App Layering/OS Machine Tools/Install.ps1
@@ -0,0 +1,49 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+$Vendor = "Citrix"
+$Product = "App Layering OS Machine Tools"
+$PackageName = "citrix_app_layering_os_machine_tools_19.11.0"
+$Version = "1911"
+$InstallerType = "exe"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$OSTools = "${env:SystemRoot}" + "\Setup\Scripts"
+$UnattendedArgs1 = '/s'
+$UnattendedArgs2 = "/S /v/qn"
+
+Start-Transcript $LogPS
+
+CD $Version
+
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs1 -Wait -Passthru).ExitCode
+
+Shutdown /a
+
+CD $OSTools
+
+Write-Verbose "Starting Installation of $Vendor Image Preparation Utility $Version" -Verbose
+(Start-Process "setup_x64.exe" -ArgumentList $UnattendedArgs2 -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
