e3df604748e0b6faafdf72e171a2a37444d44d5b
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-01-18 08:31:09 -0300

    Update Install.ps1

--- Microsoft/Sysinternals/Install.ps1
+++ Microsoft/Sysinternals/Install.ps1
@@ -5,16 +5,24 @@ $StartDTM = (Get-Date)
 $Vendor = "Windows"
 $Product = "SysinternalsSuite"
 $Version = "1.0"
+$PackageName = "SysinternalsSuite"
 $InstallerType = "zip"
+$Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $url = "https://live.sysinternals.com/Files/SysinternalsSuite.zip"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 $ProgressPreference = 'SilentlyContinue'
 
 Start-Transcript $LogPS
 
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile "${env:SystemRoot}\Temp\$Product.$InstallerType"
-Expand-Archive -Path "${env:SystemRoot}\Temp\$Product.$InstallerType" -DestinationPath "${env:SystemRoot}\System32"
+If (!(Test-Path -Path $Version)) {New-Item -ItemType directory -Path $Version | Out-Null}
+
+CD $Version
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
+
+Expand-Archive $Source -DestinationPath "${env:SystemRoot}\System32"
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
