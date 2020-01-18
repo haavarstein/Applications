47c14201aeae9d93018d599fe9d7680bef98711b
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-01-18 14:39:54 -0300

    Update Install.ps1

--- Microsoft/Edge Enterprise/Install.ps1
+++ Microsoft/Edge Enterprise/Install.ps1
@@ -38,7 +38,7 @@ Write-Verbose "Copy Preferences File" -Verbose
 CD..
 Copy-Item -Path .\master_preferences -Destination "C:\Program Files (x86)\Microsoft\Edge\Application\master_preferences" -Recurse -Force
 
-Write-Verbose "Stop and disable Microsoft Edge services" -Verbose
+Write-Verbose "Stop and Disable Microsoft Edge Services" -Verbose
 $Services = "edgeupdatem","edgeupdate","MicrosoftEdgeElevationService"
 ForEach ($Service in $Services)
 {
@@ -53,7 +53,7 @@ Set-Service -Name $Service -StartupType Disabled
 }
 }
 
-Write-Verbose "Delete Microsoft Edge scheduled tasks" -Verbose
+Write-Verbose "Delete Microsoft Edge Scheduled Tasks" -Verbose
 $EdgeScheduledTasks = "MicrosoftEdgeUpdateTaskMachineCore","MicrosoftEdgeUpdateTaskMachineUA"
 ForEach ($Task in $EdgeScheduledTasks)
 {
@@ -66,9 +66,9 @@ $RegName = "UviProcessExcludes"
 $EdgeRegvalue = "msedge.exe"
 
 # Get current values in UviProcessExcludes
-$CurrentValues = Get-ItemProperty -Path $RegPath | Select-Object -ExpandProperty $RegName
+$CurrentValues = Get-ItemProperty -Path $RegPath | Select-Object -ExpandProperty $RegName | Out-Null
 # Add the msedge.exe value to existing values in UviProcessExcludes
-Set-ItemProperty -Path $RegPath -Name $RegName -Value "$CurrentValues$EdgeRegvalue;"
+Set-ItemProperty -Path $RegPath -Name $RegName -Value "$CurrentValues$EdgeRegvalue;" | Out-Null
 
 Write-Verbose "Remove Shortcut from Public Desktop" -Verbose
 Remove-Item -Path "$env:PUBLIC\Desktop\Microsoft Edge.lnk"
