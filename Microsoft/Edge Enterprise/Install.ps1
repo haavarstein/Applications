1e238beb5a12fbbe5823b1c726e4987e80326a2c
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-01-20 18:22:33 -0300

    Update Install.ps1

--- Microsoft/Edge Enterprise/Install.ps1
+++ Microsoft/Edge Enterprise/Install.ps1
@@ -60,15 +60,17 @@ ForEach ($Task in $EdgeScheduledTasks)
 Unregister-ScheduledTask -TaskName $Task -Confirm:$false
 }
 
-Write-Verbose "Fix Citrix API Hook" -Verbose
-$RegPath = "HKLM:SYSTEM\CurrentControlSet\services\CtxUvi"
-$RegName = "UviProcessExcludes"
-$EdgeRegvalue = "msedge.exe"
+If ((Test-Path -Path HKLM:SYSTEM\CurrentControlSet\services\CtxUvi)) {
+    Write-Verbose "Fix Citrix API Hook" -Verbose
+    $RegPath = "HKLM:SYSTEM\CurrentControlSet\services\CtxUvi"
+    $RegName = "UviProcessExcludes"
+    $EdgeRegvalue = "msedge.exe"
 
-# Get current values in UviProcessExcludes
-$CurrentValues = Get-ItemProperty -Path $RegPath | Select-Object -ExpandProperty $RegName | Out-Null
-# Add the msedge.exe value to existing values in UviProcessExcludes
-Set-ItemProperty -Path $RegPath -Name $RegName -Value "$CurrentValues$EdgeRegvalue;" | Out-Null
+    # Get current values in UviProcessExcludes
+    $CurrentValues = Get-ItemProperty -Path $RegPath | Select-Object -ExpandProperty $RegName | Out-Null
+    # Add the msedge.exe value to existing values in UviProcessExcludes
+    Set-ItemProperty -Path $RegPath -Name $RegName -Value "$CurrentValues$EdgeRegvalue;" | Out-Null
+}
 
 Write-Verbose "Remove Shortcut from Public Desktop" -Verbose
 Remove-Item -Path "$env:PUBLIC\Desktop\Microsoft Edge.lnk"
