c9b98067462d219751a4cd751f1e68343b2e4aae
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-02-10 15:29:22 -0300

    Update Elevated_Logon.ps1

--- Scripts/Elevated_Logon.ps1
+++ Scripts/Elevated_Logon.ps1
@@ -2,18 +2,15 @@ Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
-$Vendor = "Set"
+$Vendor = "Enable"
 $Product = "Elevated Logon"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product.log"
 
 Start-Transcript $LogPS
 
-$MyConfigFileloc = ("$env:Settings\Applications\Settings.xml")
-[xml]$MyConfigFile = (Get-Content $MyConfigFileLoc)
-
-$ElevatedUser = $MyConfigFile.Settings.MDT.ElevatedUser
-$PasswordFile = $MyConfigFile.Settings.MDT.PasswordFile
-$KeyFile = $MyConfigFile.Settings.MDT.KeyFile
+$ElevatedUser = "xenappblog\svc-elevated"
+$PasswordFile = "elevated.txt"
+$KeyFile = "elevated.key"
 
 Write-Verbose "Getting Encrypted Password from KeyFile" -Verbose
 $SecurePassword = ((Get-Content $PasswordFile) | ConvertTo-SecureString -Key (Get-Content $KeyFile))
@@ -21,12 +18,16 @@ $SecurePasswordInMemory = [Runtime.InteropServices.Marshal]::SecureStringToBSTR(
 $PasswordAsString = [Runtime.InteropServices.Marshal]::PtrToStringBSTR($SecurePasswordInMemory)
 [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($SecurePasswordInMemory) 
 
-Write-Verbose "Creating Elevated AutoLogon"
+Write-Verbose "Creating Elevated AutoLogon" -Verbose
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d $ElevatedUser /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d $env:USERDOMAIN /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d $PasswordAsString /f
 
-#Start-Process "C:\Windows\System32\autologon.exe" -ArgumentList "/accepteula", $ElevatedUser, $env:USERDOMAIN, $PasswordAsString -Wait
+Write-Verbose "Adding $ElevatedUser to Local Admin Group" -Verbose
+Add-LocalGroupMember -Group "Administrators" -Member $ElevatedUser
+
+Write-Verbose "Disable UAC temporarily" -Verbose
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
