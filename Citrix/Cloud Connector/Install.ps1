773db344b7a3dca4394d585188f92ae2cead1d45
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-02-12 05:18:01 -0300

    Create Install.ps1

---
+++ Citrix/Cloud Connector/Install.ps1
@@ -0,0 +1,42 @@
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+$Vendor = "Citrix"
+$Product = "Cloud Connector"
+$Version = "6.16.0.31167"
+$PackageName = "cwcconnector"
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+$UnattendedArgs = '/q /Customer:trond /ClientID:XXXX /ClientSecret:XXXX /AcceptTermsOfService:true'
+$url = "https://downloads.cloud.com/trond/connector/cwcconnector.exe"
+
+Start-Transcript $LogPS
+
+if( -Not (Test-Path -Path $Version ) )
+{
+    New-Item -ItemType directory -Path $Version
+}
+
+CD $Version
+
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {
+    Invoke-WebRequest -Uri $url -OutFile $Source
+         }
+        Else {
+            Write-Verbose "File exists. Skipping Download." -Verbose
+         }
+
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
