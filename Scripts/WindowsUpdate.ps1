c0b644f3b04223a348061c0ab43a769939bc44e8
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-02-17 16:10:48 -0300

    Create WindowsUpdate.ps1

---
+++ Scripts/WindowsUpdate.ps1
@@ -0,0 +1,41 @@
+Clear-Host
+Write-Verbose "Settings Arugments"
+$StartDTM = (Get-Date)
+
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {Install-Module PSWindowsUpdate -Force | Import-Module PSWindowsUpdate}
+if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
+
+Update-Module -Name Evergreen -Force
+
+Write-Verbose "Checking if the Windows Update Service is Running" -Verbose
+$ServiceName = 'wuauserv'
+Set-Service -Name $ServiceName -Startup Automatic
+Start-Service -Name $ServiceName
+
+Write-Verbose "Gettings Available Updates from Microsoft" -Verbos
+Get-WindowsUpdate -NotCategory "Drivers" -MicrosoftUpdate -ComputerName localhost | Out-File C:\PSWindowsUpdate.log -Append
+
+Write-Verbose "Installing Available Updates from Microsoft" -Verbose
+Get-WindowsUpdate -NotCategory "Drivers" -MicrosoftUpdate -ComputerName localhost -Install -AcceptAll -IgnoreReboot | Out-File C:\PSWindowsUpdate.log -Append
+
+Write-Verbose "Updating Antivirus Signatures" -Verbose
+if( (Test-Path -Path "C:\Program Files (x86)\Symantec\Symantec Endpoint Protection" ) )
+{
+    Start-Process -FilePath "C:\Program Files (x86)\Symantec\Symantec Endpoint Protection\SepLiveUpdate.exe" -PassThru -Wait -RedirectStandardOutput SEP.txt
+    $SEP = Get-Content .\SEP.txt
+    Write-Verbose "$SEP" -Verbose
+}
+
+if( (Get-Service WinDefend| Where-Object {$_.Status -eq "Running"}) )
+{
+    CD "C:\Program Files\Windows Defender\"
+    .\MpCmdRun.exe -signatureupdate
+}
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
