e4135625ac4bcea778e12ba78cf16471c112fca4
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-02-25 11:55:56 -0300

    Update Install.ps1

--- Misc/WinSCP/Install.ps1
+++ Misc/WinSCP/Install.ps1
@@ -73,7 +73,7 @@ $Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
-$UnattendedArgs = '/verysilent'
+$UnattendedArgs = '/verysilent /ALLUSERS'
 $url = "https://ufpr.dl.sourceforge.net/project/winscp/WinSCP/$Version/WinSCP-$Version-Setup.exe"
 $ProgressPreference = 'SilentlyContinue'
 
