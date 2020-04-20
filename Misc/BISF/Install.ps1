80eb01e67dc07c84a8f3aacdebc4c227f02b0f25
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-04-20 07:21:06 -0300

    Update Install.ps1

--- Misc/BISF/Install.ps1
+++ Misc/BISF/Install.ps1
@@ -76,11 +76,12 @@ Write-Verbose "Customization" -Verbose
 New-Item -ItemType directory -Path "C:\Program Files\Citrix Optimizer\" | Out-Null
 Copy-Item -Path .\CitrixOptimizer\* -Destination "C:\Program Files\Citrix Optimizer\" -Recurse -Force
 Copy-Item -Path .\Tools\* -Destination $env:SystemRoot\System32 -Recurse -Force
+Copy-Item -Path .\PREP_custom\*.ps1 -Destination "C:\Program Files (x86)\Base Image Script Framework (BIS-F)\Framework\SubCall\Preparation\Custom" -Recurse -Force
+Copy-Item -Path .\PERS_custom\*.ps1 -Destination "C:\Program Files (x86)\Base Image Script Framework (BIS-F)\Framework\SubCall\Personalization\Custom" -Recurse -Force
+
 #CD..
 #Copy-Item BISF.reg -Destination C:\Windows\Temp\BISF.reg -Recurse
 #cmd.exe /c "regedit /s C:\Windows\Temp\BISF.reg"
-#Copy-Item -Path .\PREP_custom\*.ps1 -Destination "C:\Program Files (x86)\Base Image Script Framework (BIS-F)\Framework\SubCall\Preparation\Custom" -Recurse -Force
-#Copy-Item -Path .\PERS_custom\*.ps1 -Destination "C:\Program Files (x86)\Base Image Script Framework (BIS-F)\Framework\SubCall\Personalization\Custom" -Recurse -Force
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
