59b44bc8437f00cd97efa264da9e27a75789f470
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-04-30 07:57:36 -0300

    Update Install.ps1

--- Citrix/Cloud Connector/Install.ps1
+++ Citrix/Cloud Connector/Install.ps1
@@ -1,34 +1,53 @@
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
+$CustomerID = "XXX"
+$ClientID = "XXX"
+$ClientSecret = "XXX"
+
 $Vendor = "Citrix"
 $Product = "Cloud Connector"
-$Version = "6.16.0.31167"
 $PackageName = "cwcconnector"
 $InstallerType = "exe"
 $Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
-$UnattendedArgs = '/q /Customer:trond /ClientID:XXXX /ClientSecret:XXXX /AcceptTermsOfService:true'
-$url = "https://downloads.cloud.com/trond/connector/cwcconnector.exe"
+$url = "https://downloads.cloud.com/$CustomerID/connector/cwcconnector.exe"
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Start-Transcript $LogPS
-
-if( -Not (Test-Path -Path $Version ) )
-{
-    New-Item -ItemType directory -Path $Version
-}
-
-CD $Version
 
 Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
 If (!(Test-Path -Path $Source)) {
     Invoke-WebRequest -Uri $url -OutFile $Source
-         }
-        Else {
-            Write-Verbose "File exists. Skipping Download." -Verbose
-         }
+    $Version = (Get-Command .\$Source).FileVersionInfo.FileVersion
+}
+
+If (!(Test-Path -Path $Version)) {
+    New-Item -ItemType directory -Path $Version | Out-Null
+    Copy-Item $Source -Destination $Version
+    Remove-Item $Source
+}
+
+# Using System Environment Variable Location to defect Resource Location - Set via Group Policy Preferences
+Write-Verbose "Getting Resource Location" -Verbose
+
+if (($env:Location -eq "HQ")) {
+
+     Write-Verbose "Resource Location is $env:Location" -Verbose
+     $UnattendedArgs = '/q /Customer:'+$CustomerID+' /ClientID:'+$ClientID+' /ClientSecret:'+$ClientSecret+' /ResourceLocationId:XXX /AcceptTermsOfService:true'
+}
+
+if (($env:Location -eq "Azure")) {
+
+     Write-Verbose "Resource Location is $env:Location" -Verbose
+     $UnattendedArgs = '/q /Customer:'+$CustomerID+' /ClientID:'+$ClientID+' /ClientSecret:'+$ClientSecret+' /ResourceLocationId:XXX /AcceptTermsOfService:true'
+}
+
+CD $Version
 
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
 (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
