621fd36f4b4ba0e5947f73bd82d1b6ad011fc3a3
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-05-29 07:58:07 -0300

    Update Install.ps1

--- Misc/NotePadPlusPlus/Install.ps1
+++ Misc/NotePadPlusPlus/Install.ps1
@@ -28,6 +28,7 @@ $Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 $ProgressPreference = 'SilentlyContinue'
 $UnattendedArgs = '/S'
 
