51cdf5f9804d3fdb9c8e145383921a9d06f7a4a6
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-06-08 08:14:08 -0300

    Update Set-StaticIP.ps1

--- Scripts/Set-StaticIP.ps1
+++ Scripts/Set-StaticIP.ps1
@@ -1,5 +1,3 @@
-# Needs to run as PowerShell and NOT as an Application in the Task Sequence (Module is not loaded)
-
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
@@ -9,36 +7,9 @@ $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product PS Wrapper.log"
 
 Start-Transcript $LogPS
 
-Write-Verbose "Importing PowerShell Module" -Verbose
-Import-Module ZTIUtility.psm1
-
-Write-Verbose "Using COM interop to access the TSEnvironment object" -Verbose
-$TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
-
-Write-Verbose "Getting Task Sequence Variables" -Verbose
-$DeployRoot      = $TSEnv.Value("DeployRoot")
-$OSDComputerName = $TSEnv.Value("OSDComputerName")
-$MachineObjectOU = $TSEnv.Value("MachineObjectOU")
-$MacAddress  = $TSEnv.Value("MacAddress")
-$IPAddress  = $TSEnv.Value("OSDAdapter0IPAddressList")
-$Subnet  = $TSEnv.Value("OSDAdapter0SubnetMask")
-$Gateway  = $TSEnv.Value("OSDAdapter0Gateways")
-$DNS  = $TSEnv.Value("OSDAdapter0DNSServerList")
-$FQDN  = $TSEnv.Value("OSDAdapter0DNSSuffi")
-
-Write-Verbose "Listing Task Sequence Variables" -Verbose
-Write-Host $DeployRoot
-Write-Host $OSDComputerName
-Write-Host $MachineObjectOU
-Write-Host $MacAddress
-Write-Host $IPAddress
-Write-Host $Subnet
-Write-Host $Gateway
-Write-Host $DNS
-Write-Host $FQDN
-
 Write-Verbose "Getting Static IP Address" -Verbose
-New-NetIPAddress -InterfaceAlias Ethernet0 -IPv4Address $IPAddress -PrefixLength 24 -DefaultGateway $Gateway
+New-NetIPAddress -InterfaceAlias Ethernet0 -IPAddress 192.168.86.16 -PrefixLength 24 -DefaultGateway 192.168.86.254
+Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses ("192.168.86.10","192.168.86.11")
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
