9fe23ef1ae4016244210769077e56e1df3a39ef1
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-07-14 13:39:57 -0300

    Update Install_Agent.ps1

--- Citrix/Workspace Environment Management/Install_Agent.ps1
+++ Citrix/Workspace Environment Management/Install_Agent.ps1
@@ -1,17 +1,14 @@
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
-
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
-
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
-
 # Example 3 EXE (Always use ' '):
 # $UnattendedArgs='/qn'
 # (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
-
 # Example 4 MSI (Always use " "):
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
@@ -20,7 +17,7 @@ $MyConfigFileloc = ("$env:Settings\Applications\Settings.xml")
 
 $Vendor = "Citrix"
 $Product = "Workspace Environment Management Agent"
-$PackageName = "Citrix Workspace Environment Management Agent"
+$PackageName = "Citrix Workspace Environment Management Agent Setup"
 $InstallerType = "exe"
 $Version = "2005"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
