0f1e5749877d8dd08c1f0c9d5a2acb8e1ad83f8c
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-07-17 09:50:55 -0300

    Update VMware_Horizon_Connection_Server_IP.ps1

--- Scripts/VMware_Horizon_Connection_Server_IP.ps1
+++ Scripts/VMware_Horizon_Connection_Server_IP.ps1
@@ -1,7 +1,4 @@
-#Static Network Address VMWare View Connection Server
-#New-NetIPAddress -InterfaceIndex 12 -IPAddress 192.168.1.15 -PrefixLength 24 -DefaultGateway 192.168.1.1
-#Set-DNSClientServerAddress -InterfaceIndex 12 -ServerAddresses ("192.168.1.10")
-
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
@@ -14,14 +11,15 @@ $Product = "Horizon Connection Server IP Address"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product PS Wrapper.log"
 
 $NIC = Get-WMIObject Win32_NetworkAdapterConfiguration -computername . | where{$_.IPEnabled -eq $true -and $_.DHCPEnabled -eq $true}
-$GW = $nic.DefaultIPGateway | Select-Object -First 1 | Out-String
+$GW = $NIC.DefaultIPGateway | Where-Object { $_ -match '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$' }
 $IPAddress = $MyConfigFile.Settings.VMware.ConnectionServerIP
 
 Start-Transcript $LogPS
 
 Write-Verbose "Getting Static IP Address" -Verbose
 New-NetIPAddress -InterfaceAlias Ethernet0 -IPAddress $IPAddress -PrefixLength 24 -DefaultGateway $GW.Trim()
-Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses ("192.168.1.10","192.168.1.20")
+Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses $NIC.DNSServerSearchOrder
+#Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses ("192.168.1.10","192.168.1.20")
 
 Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
