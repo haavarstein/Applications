780df19e39709ae2d5ce1c0cabc0c3a046908461
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-07-17 17:51:55 -0300

    Update Install.ps1

--- Misc/NotePadPlusPlus/Install.ps1
+++ Misc/NotePadPlusPlus/Install.ps1
@@ -14,25 +14,23 @@ $StartDTM = (Get-Date)
 
 Write-Verbose "Installing Modules" -Verbose
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Install-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
 if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
 Update-Module Evergreen
 
-$Vendor = "SmartX"
-$Product = "ControlUp Agent"
-$Evergreen = Get-ControlUpAgent | Where-Object { $_.Framework -eq "net45" -and $_.Architecture -eq "x64" } | Select-Object -First 1
+$Vendor = "Misc"
+$Product = "NotePadPlusPlus"
+$PackageName = "NotePadPlusPlus_x64"
+$Evergreen = Get-NotepadPlusPlus | Where-Object {$_.Architecture -eq "x64" -and $_.URI -like "*.exe" }
 $Version = $Evergreen.Version
 $URL = $Evergreen.uri
-$PackageName = "CUPAgent"
-$InstallerType = "msi"
+$InstallerType = "exe"
 $Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
-$UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 $ProgressPreference = 'SilentlyContinue'
-$ServiceName = "cuagent"
+$UnattendedArgs = '/S'
 
 Start-Transcript $LogPS | Out-Null
  
@@ -42,32 +40,9 @@ CD $Version
  
 Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
 If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
-
-Write-Verbose "Checking if $Vendor $Product Service is Running" -Verbose
-
-If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
-
-    If ((Get-Service $serviceName).Status -eq 'Running') {
-
-        Set-Service -Name $serviceName -StartupType Disabled -Status Stopped 
-        Write-Verbose "Stopping and Disabling $serviceName" -Verbose
-
-    } Else {
-
-        Write-Verbose "$serviceName found, but it is not running" -Verbose
-
-    }
-
-} Else {
-
-    Write-Verbose "$serviceName not found" -Verbose
-
-}
-
+        
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
-(Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
-Set-Service -Name $serviceName -StartupType Automatic
-Start-Service -Name $serviceName
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
 
 Write-Verbose "Customization" -Verbose
 
