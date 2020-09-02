926e837196943f133f9fddb82cbc6c9c42946c06
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-09-02 03:38:24 -0300

    Update Install.ps1

--- Citrix/App Layering/OS Machine Tools/Install.ps1
+++ Citrix/App Layering/OS Machine Tools/Install.ps1
@@ -1,24 +1,21 @@
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
-
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
-
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
-
 # Example 3 EXE (Always use ' '):
 # $UnattendedArgs='/qn'
 # (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
-
 # Example 4 MSI (Always use " "):
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
 $Vendor = "Citrix"
 $Product = "App Layering OS Machine Tools"
-$PackageName = "citrix_app_layering_os_machine_tools_19.11.0"
-$Version = "1911"
+$PackageName = "citrix_app_layering_os_machine_tools_20.8.0"
+$Version = "2008"
 $InstallerType = "exe"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
