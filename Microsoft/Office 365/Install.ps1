9e0bbd3f19f385c20b19258de4a1c048388cf3d8
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-09-05 12:53:06 -0300

    Update Install.ps1

--- Microsoft/Office 365/Install.ps1
+++ Microsoft/Office 365/Install.ps1
@@ -1,29 +1,3 @@
-function Get-ODTUri {
-    <#
-        .NOTES
-            Author: Bronson Magnan
-            Twitter: @cit_bronson
-            Modified by: Marco Hofmann
-            Twitter: @xenadmin
-    #>
-    [CmdletBinding()]
-    [OutputType([string])]
-    param ()
-
-    $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
-    try {
-        $response = Invoke-WebRequest -UseBasicParsing -Uri $url -ErrorAction SilentlyContinue
-    }
-    catch {
-        Throw "Failed to connect to ODT: $url with error $_."
-        Break
-    }
-    finally {
-        $ODTUri = $response.links | Where-Object {$_.outerHTML -like "*click here to download manually*"}
-        Write-Output $ODTUri.href
-    }
-}
-
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
@@ -38,56 +12,40 @@ Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
+Write-Verbose "Installing Modules" -Verbose
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Install-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
+Update-Module Evergreen
+
 $Vendor = "Microsoft"
-$Product = "Office 365 x32"
+$Product = "Office 365 x64"
 $PackageName = "setup"
+$Evergreen = Get-MicrosoftOffice | Where-Object {$_.Channel -eq "Monthly"}
+$Version = $Evergreen.Version
+$URL = $Evergreen.uri
 $InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
 $Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
 $UnattendedArgs = '/configure RDSH.xml'
 $UnattendedArgs2 = '/download RDSH.xml'
-$URL = $(Get-ODTUri)
 $ProgressPreference = 'SilentlyContinue'
 
 Start-Transcript $LogPS | Out-Null
-
-Write-Verbose "Checking Internet Connection" -Verbose
  
-If (!(Test-Connection -ComputerName www.google.com -Count 1 -quiet)) {
-    Write-Verbose "Internet Connection is Down" -Verbose
-    }
-    Else {
-    Write-Verbose "Internet Connection is Up" -Verbose
-    }
-
-Write-Verbose "Downloading Latest Version of Office 365 Deployment Tool (ODT)" -Verbose
-Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile .\officedeploymenttool.exe
-$Version = (Get-Command .\officedeploymenttool.exe).FileVersionInfo.FileVersion
-
-Write-Verbose "Writing Version Number to File" -Verbose
-if (!$Version) {
-    $Version = Get-Content -Path ".\Version.txt"
-    }
-    Else {
-    $Version | Out-File -FilePath ".\Version.txt" -Force
-    }
-
 if( -Not (Test-Path -Path $Version ) )
 {
     New-Item -ItemType directory -Path $Version | Out-Null
-    Copy-item .\RDSH.xml -Destination $Version -Force
-    $Dir = Get-Location
-    $Path = "$Dir" + "\$Version"
-    .\officedeploymenttool.exe /quiet /extract:.\$Version
-    $Version | Out-File -FilePath ".\Version.txt" -Force
-    start-sleep -s 5
+    Copy-item $PSScriptRoot\RDSH.xml -Destination $Version -Force
+    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Version\$Source
 }
-
+ 
 CD $Version
 
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
 If (!(Test-Path -Path $PSScriptRoot\$Version\Office\Data\v32.cab)) {
-    Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
     (Start-Process "Setup.exe" -ArgumentList $unattendedArgs2 -Wait -Passthru).ExitCode
          }
         Else {
@@ -103,4 +61,4 @@ Write-Verbose "Stop logging" -Verbose
 $EndDTM = (Get-Date)
 Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
 Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
-Stop-Transcript | Out-Null
+Stop-Transcript
