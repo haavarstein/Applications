49db4b2da7ebed9c6ef151f54d374fb71e4701a1
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-09-28 11:41:58 -0300

    Create Uninstall.ps1

---
+++ Veeam/Agent/Uninstall.ps1
@@ -0,0 +1,11 @@
+$app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Veeam Agent for Microsoft Windows'"
+$app.Uninstall()
+
+$app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Microsoft SQL Server 2012 Express LocalDB'"
+$app.Uninstall()
+
+$app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Microsoft SQL Server 2012 Management Objects  (x64)'"
+$app.Uninstall()
+
+$app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Microsoft System CLR Types for SQL Server 2012 (x64)'"
+$app.Uninstall()
