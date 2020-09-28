ea7705617463c5e6fd191aa4fb8285a4263ae7ec
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-09-28 11:39:06 -0300

    Create Install.ps1

---
+++ Veeam/Agent/Install.ps1
@@ -0,0 +1,44 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+$Vendor = "Veeam"
+$Product = "Agent"
+$PackageName = "VeeamAgentWindows_4.0.0.1811"
+$Version = "4.0.0.1811"
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+$ProgressPreference = 'SilentlyContinue'
+$UnattendedArgs = '/silent /accepteula /acceptthirdpartylicenses'
+
+Start-Transcript $LogPS | Out-Null
+
+CD $Version
+    
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+$t = Start-Process -FilePath .\$PackageName.$InstallerType -ArgumentList $UnattendedArgs -PassThru -ErrorAction Stop
+if($t -ne $null)
+{
+   Wait-Process -InputObject $t
+}
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
