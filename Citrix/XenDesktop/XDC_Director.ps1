54f7d01d3f5705a167c8e0c4ca57f0978775f9fb
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-10-03 07:42:24 -0300

    Update XDC_Director.ps1

--- Citrix/XenDesktop/XDC_Director.ps1
+++ Citrix/XenDesktop/XDC_Director.ps1
@@ -1,17 +1,14 @@
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
-
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
-
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
-
 # Example 3 EXE (Always use ' '):
 # $UnattendedArgs='/qn'
 # (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
-
 # Example 4 MSI (Always use " "):
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
@@ -25,7 +22,7 @@ $InstallerType = "exe"
 $Version = $MyConfigFile.Settings.Citrix.Version
 $LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $PackageName $Version PS Wrapper.log"
 $LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
-$UnattendedArgs = '/quiet /configure_firewall /COMPONENTS CONTROLLER,DESKTOPDIRECTOR /nosql /ignore_hw_check_failure /logpath $LogApp /noreboot'
+$UnattendedArgs = '/quiet /configure_firewall /COMPONENTS STUDIO,CONTROLLER,DESKTOPDIRECTOR /nosql /ignore_hw_check_failure /logpath $LogApp /noreboot'
 $Destination = "$Version\x64\XenDesktop Setup\"
 
 Start-Transcript $LogPS
