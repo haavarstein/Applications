57c67a5305d06c2bab83b2046611ea844e7f65d3
Trond Eirik Haavarstein <haavarstein@gmail.com>
2020-11-09 02:21:29 -0800

    Add files via upload

---
+++ Parallels/Remote Application Server/Create Win32 Parallels Client.ps1
@@ -0,0 +1,73 @@
+﻿# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+$TentantID = "XXXXX"
+$Vendor = "Parallels"
+$Product = "Client"
+$PackageName = "RASClient-x64"
+$InstallerType = "msi"
+$Source = "$PackageName" + "." + "$InstallerType"
+$Path = Get-Location
+$Path = $Path.Path
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$Product.log"
+$Destination = "${env:ChocoRepository}" + "\$Vendor\$Product\$Version\$packageName.$installerType"
+$UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+$url = "http://download.parallels.com/ras/latest/RASClient-x64.msi"
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+Start-Transcript $LogPS | Out-Null
+
+Write-Verbose "Installing Modules" -Verbose
+if (!(Get-Module -ListAvailable -Name IntuneWin32App)) {Install-Module IntuneWin32App -Force | Import-Module IntuneWin32App}
+Connect-MSIntuneGraph -TenantName $TentantID
+
+Write-Verbose "Downloading $Vendor $Product" -Verbose
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
+
+Write-Verbose "Creating Win32 Intune Package" -Verbose
+$Win32AppPackage = New-IntuneWin32AppPackage -SourceFolder $Path -SetupFile $Source -OutputFolder $Path -Verbose
+
+# Get MSI meta data from .intunewin file
+$IntuneWinFile = $Win32AppPackage.Path
+$IntuneWinMetaData = Get-IntuneWin32AppMetaData -FilePath $IntuneWinFile
+
+# Create custom display name like 'Name' and 'Version'
+$DisplayName = $IntuneWinMetaData.ApplicationInfo.Name + " " + $IntuneWinMetaData.ApplicationInfo.MsiInfo.MsiProductVersion
+$Publisher = $IntuneWinMetaData.ApplicationInfo.MsiInfo.MsiPublisher
+
+# Create MSI detection rule
+$DetectionRule = New-IntuneWin32AppDetectionRuleMSI -ProductCode $IntuneWinMetaData.ApplicationInfo.MsiInfo.MsiProductCode -ProductVersionOperator "greaterThanOrEqual" -ProductVersion $IntuneWinMetaData.ApplicationInfo.MsiInfo.MsiProductVersion
+
+# Create custom return code
+$ReturnCode = New-IntuneWin32AppReturnCode -ReturnCode 1337 -Type "retry"
+
+# Convert image file to icon
+$ImageFile = "$Path\$Vendor.png"
+$Icon = New-IntuneWin32AppIcon -FilePath $ImageFile
+
+# Add new MSI Win32 app
+$Win32App = Add-IntuneWin32App -FilePath $IntuneWinFile -DisplayName $DisplayName -Description "Install $Vendor $Product application" -Publisher $Publisher -InstallExperience "system" -RestartBehavior "suppress" -DetectionRule $DetectionRule -ReturnCode $ReturnCode -Icon $Icon -Verbose
+
+# Add assignment for all users
+Add-IntuneWin32AppAssignmentAllUsers -ID $Win32App.id -Intent "available" -Notification "showAll" -Verbose
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript | Out-Null
