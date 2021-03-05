e457dd8550a06ba6db46ca95f075cc4ef0eee30b
Trond Eirik Haavarstein <haavarstein@gmail.com>
2021-03-05 08:25:49 -0300

    Update Install_Console.ps1

--- Misc/ControlUp/Install_Console.ps1
+++ Misc/ControlUp/Install_Console.ps1
@@ -1,52 +1,3 @@
-# https://github.com/BronsonMagnan/SoftwareUpdate/blob/master/ControlUpAgent.ps1
-
-function Get-CurrentControlUpAgentVersion {
-    [cmdletbinding()]
-    [outputType([version])]
-    param()
-    $agentURL = "http://www.controlup.com/products/controlup/agent/"
-    #ControlUP forces TLS 1.2 and rejects TLS 1.1
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $webrequest = wget -Uri $agentURL -UseBasicParsing
-    $content = $webrequest.Content
-    #clean up the code into paragraph blocks
-    $paragraphSections = $content.replace("`n","").replace("  ","").replace("`t","").replace("<p>","#$%^<p>").split("#$%^").trim()
-    #now we are looking for the pattern <p><strong>Current agent version:</strong> 7.2.1.6</p>
-    $versionLine = $paragraphSections  | where {$_ -like "*Current*agent*"}
-    $splitlines = ($versionLine.replace('<','#$%^<').replace('>','>#$%^').split('#$%^')).trim()
-    $pattern = "(\d+\.){3}\d+"
-    $version = [Version]::new(($splitlines | select-string -Pattern $pattern).tostring())
-    Write-Output $version
-}
-
-function Get-CurrentControlUpAgentURL {
-    [cmdletBinding()]
-    [outputType([string])]
-    param(
-        [validateSet("net45","net35")]
-        [string]$netversion = "net45",
-        [validateSet("x86","x64")]
-        [string]$architecture = "x64"
-    )
-    $version = Get-CurrentControlUpAgentVersion
-    $DownloadURL = "https://downloads.controlup.com/agent/$($version.tostring())/ControlUpAgent-$($netversion)-$($architecture)-$($version).msi"
-    Write-Output $DownloadURL
-}
-
-function Get-CurrentControlUpConsoleURL {
-    [cmdletBinding()]
-    [outputType([string])]
-    param(
-        [validateSet("net45","net35")]
-        [string]$netversion = "net45",
-        [validateSet("x86","x64")]
-        [string]$architecture = "x64"
-    )
-    $version = Get-CurrentControlUpAgentVersion
-    $DownloadURL = "https://downloads.controlup.com/console/$($version.tostring())/ControlUp.zip"
-    Write-Output $DownloadURL
-}
-
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
@@ -57,12 +8,15 @@ function Get-CurrentControlUpConsoleURL {
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
 
+Clear-Host
 Write-Verbose "Setting Arguments" -Verbose
 $StartDTM = (Get-Date)
 
 $Vendor = "SmartX"
 $Product = "ControlUp Console x64"
-$Version = "$(Get-CurrentControlUpAgentVersion)"
+$Evergreen = Get-ControlUpConsole
+$Version = $Evergreen.version
+$URL = $Evergreen.uri
 $PackageName = "ControlUpConsole"
 $InstallerType = "zip"
 $Source = "$PackageName" + "." + "$InstallerType"
