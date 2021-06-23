573bf27039f64c1fd6accf9b08845b8c3a62b3cb
Trond Eirik Haavarstein <haavarstein@gmail.com>
2021-06-23 08:38:45 -0300

    Update WindowsUpdate.ps1

--- Scripts/WindowsUpdate.ps1
+++ Scripts/WindowsUpdate.ps1
@@ -2,16 +2,23 @@ Clear-Host
 Write-Verbose "Settings Arugments"
 $StartDTM = (Get-Date)
 
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-Install-PackageProvider -Name 'Nuget' -Force
-if (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {Install-Module PSWindowsUpdate -Scope CurrentUser -Force | Import-Module PSWindowsUpdate}
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {Install-Module PSWindowsUpdate -Force | Import-Module PSWindowsUpdate}
+if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
+
+Update-Module -Name Evergreen -Force
 
 Write-Verbose "Checking if the Windows Update Service is Running" -Verbose
 $ServiceName = 'wuauserv'
 Set-Service -Name $ServiceName -Startup Automatic
 Start-Service -Name $ServiceName
 
-Write-Verbose "Gettings Available Updates from Microsoft" -Verbos
+Write-Verbose "Deleting FSLogix Rules" -Verbose
+Remove-Item -Path "C:\Program Files\FSLogix\Apps\Rules\*.*" -Force -Recurse
+
+Write-Verbose "Gettings Available Updates from Microsoft" -Verbose
 Get-WindowsUpdate -NotCategory "Drivers" -MicrosoftUpdate -ComputerName localhost | Out-File C:\PSWindowsUpdate.log -Append
 
 Write-Verbose "Installing Available Updates from Microsoft" -Verbose
