00b0899449dc963929b4e8aeea00f892562cfc32
Trond Eirik Haavarstein <haavarstein@gmail.com>
2022-05-19 15:09:52 -0300

    Create Install.ps1

---
+++ Misc/Datto/Cloud Continuity/Install.ps1
@@ -0,0 +1,46 @@
+# PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
+# Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
+# Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
+# Example 3 EXE (Always use ' '):
+# $UnattendedArgs='/qn'
+# (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+# Example 4 MSI (Always use " "):
+# $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
+# (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
+
+Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+Write-Verbose "Installing Modules" -Verbose
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies}
+if (!(Get-Module -ListAvailable -Name Evergreen)) {Install-Module Evergreen -Force | Import-Module Evergreen}
+Update-Module Evergreen
+
+$Vendor = "Datto"
+$Product = "Cloud Continuity"
+$PackageName = "DattoCloudContinuityInstaller"
+$URL = "https://cf-dl.datto.com/cloudcontinuity/DattoCloudContinuityInstaller.exe"
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product PS Wrapper.log"
+$LogApp = "${env:SystemRoot}" + "\Temp\$PackageName.log"
+$ProgressPreference = 'SilentlyContinue'
+$UnattendedArgs = '/install /quiet /norestart'
+
+Start-Transcript $LogPS | Out-Null
+ 
+Write-Verbose "Downloading $Vendor $Product" -Verbose
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
+        
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Customization" -Verbose
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $([math]::Round( ($EndDTM-$StartDTM).TotalSeconds )) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $([math]::Round( ($EndDTM-$StartDTM).TotalMinutes )) Minutes" -Verbose
+Stop-Transcript
