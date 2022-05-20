c3de51193f7f5ba9c8a40c774e027995e188a769
Trond Eirik Haavarstein <haavarstein@gmail.com>
2022-05-20 19:40:23 -0300

    Update DHCP-ESXi.ps1

--- Scripts/DHCP-ESXi.ps1
+++ Scripts/DHCP-ESXi.ps1
@@ -23,10 +23,11 @@ Install-WindowsFeature -Name "DHCP" -IncludeManagementTools
 netsh dhcp add securitygroups
 Restart-service dhcpserver
 Add-DhcpServerInDC -DnsName $Env:COMPUTERNAME
-Set-ItemProperty –Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 –Name ConfigurationState –Value 2
+Set-ItemProperty â€“Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 â€“Name ConfigurationState â€“Value 2
 Add-DhcpServerV4Scope -Name "DHCP Scope" -StartRange $StartRange -EndRange $EndRange -SubnetMask $Subnet
 Set-DhcpServerV4OptionValue -DnsDomain $DomainFQDN -DnsServer $DNSServerIP -Router $Router
-Set-DhcpServerv4OptionValue -OptionId 66 -Value "192.168.10.9" -ComputerName $Env:COMPUTERNAME
+Set-DhcpServerv4OptionValue -OptionId 6 -Value $DNSServerIP -ComputerName $Env:COMPUTERNAME
+Set-DhcpServerv4OptionValue -OptionId 66 -Value "192.168.10.12" -ComputerName $Env:COMPUTERNAME
 Set-DhcpServerv4OptionValue -OptionId 67 -Value "\boot\x64\wdsnbp.com" -ComputerName $Env:COMPUTERNAME			
 Set-DhcpServerv4Scope -ScopeId $DHCPServerIP -LeaseDuration 1.00:00:00
 
