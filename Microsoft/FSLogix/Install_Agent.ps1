04c5fb86c129f3e273cf72b2ad11868047d3db89
Trond Eirik Haavarstein <haavarstein@gmail.com>
2022-05-30 15:33:37 -0300

    Update Install_Agent.ps1

--- Microsoft/FSLogix/Install_Agent.ps1
+++ Microsoft/FSLogix/Install_Agent.ps1
@@ -23,7 +23,7 @@ Update-Module Evergreen
 $Vendor = "Microsoft"
 $Product = "FSLogix Apps"
 $PackageName = "FSLogixAppsSetup"
-$Evergreen = Get-EvergreenApp -Name MicrosoftFSLogixApps | Where-Object { $_.Channel -eq "Preview" }
+$Evergreen = Get-EvergreenApp -Name MicrosoftFSLogixApps
 $Version = $Evergreen.Version
 $URL = $Evergreen.uri
 $DownloadType = "zip"
