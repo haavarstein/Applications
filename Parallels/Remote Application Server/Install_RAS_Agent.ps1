bb607b15ea00b04828875d35ad1aad5cc01a753d
Trond Eirik Haavarstein <haavarstein@gmail.com>
2023-06-26 10:42:59 -0300

    Update Install_RAS_Agent.ps1

--- Parallels/Remote Application Server/Install_RAS_Agent.ps1
+++ Parallels/Remote Application Server/Install_RAS_Agent.ps1
@@ -1,13 +1,9 @@
 # PowerShell Wrapper for MDT, Standalone and Chocolatey Installation - (C)2015 xenappblog.com 
-
 # Example 1: Start-Process "XenDesktopServerSetup.exe" -ArgumentList $unattendedArgs -Wait -Passthru
-
 # Example 2 Powershell: Start-Process powershell.exe -ExecutionPolicy bypass -file $Destination
-
 # Example 3 EXE (Always use ' '):
 # $UnattendedArgs='/qn'
 # (Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
-
 # Example 4 MSI (Always use " "):
 # $UnattendedArgs = "/i $PackageName.$InstallerType ALLUSERS=1 /qn /liewa $LogApp"
 # (Start-Process msiexec.exe -ArgumentList $UnattendedArgs -Wait -Passthru).ExitCode
@@ -43,7 +39,7 @@ If (!(Test-Path -Path $Source)) {
          }
 
 Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
-(Start-Process msiexec.exe -ArgumentList "/i $PackageName.$InstallerType /qn ADDLOCAL=F_Agent,F_PowerShell ADDFWRULES=1" -Wait -Passthru).ExitCode
+(Start-Process msiexec.exe -ArgumentList "/i $PackageName.$InstallerType /qn /norestart ADDLOCAL=F_Agent,F_PowerShell ADDFWRULES=1" -Wait -Passthru).ExitCode
 
 Write-Verbose "Customization" -Verbose
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "$env:userdomain\Domain Users"
